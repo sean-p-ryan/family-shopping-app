e6863340ab5a9c7688bbd1dbc1b300ee7229a63a
Sean Ryan <seanpryan89@gmail.com>
2019-07-12 19:31:36 -0400

    folder structure changed

---
+++ client/components/EditItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class EditItem extends Component {
+    render() {
+        return (
+            <div>
+                <p>Welcome to the Edit Item Component!!</p>
+            </div>
+        )
+    }
+}
